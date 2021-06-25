796dcb7c18d4583862d4d51b540176ddf23d844c
connorworthen <connorw220@protonmail.com>
2021-06-25 16:01:39 -0700

    break

--- src/user/userProfile.js
+++ src/user/userProfile.js
@@ -19,7 +19,6 @@ class Profile {
       .then((data) => {
         let userData = data
         Profile.displayForm(userData)
-        // document.getElementById("firstUpdate").innerHTML = `${data.first}`
       })
   }
 
@@ -27,6 +26,7 @@ class Profile {
     profileBtn.addEventListener('click', () => {
       document.getElementById("profileModal").style.display = "block"
       Profile.grabUserData()
+      Profile.submitHandler();
     })
   }
 
@@ -37,18 +37,59 @@ class Profile {
                   <h2 class="formTitle">Update Profile</h2>                               
                   <input type="text" id="firstUpdate" value=${userData.first} />
 
-                  <input type="text" id="last" />
+                  <input type="text" id="last" value=${userData.last} />
 
-                  <input type="text" id="email" placeholder="Email *" />
+                  <input type="text" id="email" value=${userData.email} />
 
-                  <input type="text" id="phone" placeholder="Phone Number *" />
+                  <input type="text" id="phone" value=${userData.phone} />
 
-                  <input type="text" id="address" placeholder="Address *" />
+                  <input type="text" id="address" value=${userData.address}/>
 
-                  <input type="password" id="password" placeholder="Password *" />
+                  <input type="password" id="password" value=${userData.password} />
 
-                  <input type="submit" id="form-box" value="Create Account" />
+                  <input type="submit" id="patchBox" value="Update Account" />
               </form>
       `
+  }
+
+  static submitHandler() {
+    document.getElementById("patchBox").onsubmit = (e) => {
+      e.preventDefault()
+      Profile.patchUpdateForm(
+        e.target['first'].value,
+        e.target['last'].value,
+        e.target['email'].value,
+        e.target['phone'].value,
+        e.target['address'].value,
+        e.target['password'].value
+      )
+      return false
+    }
+  }
+
+  static patchUpdateForm(first, last, email, phone, address, password) {
+
+    let configObj = {
+      method: "PATCH",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+
+      body: JSON.stringify({    
+        user: {
+          first,
+          last,
+          email,
+          phone,
+          address, 
+          password
+        }  
+      })
+    };
+    fetch(`http://localhost:3000/users/${id}`, configObj)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("hello", data)
+      })
   }
 }
