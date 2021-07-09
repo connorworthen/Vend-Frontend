f612985b7c4732b823dd55924357d25bbbe0501f
connorworthen <connorw220@protonmail.com>
2021-07-09 16:14:18 -0700

    submit creation of store

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -11,8 +11,32 @@ class Store {
   }
 
   static postClickStore () {
-    createStoreForm.addEventListener('submit', (e) => {
-      console.log("test")
+    createStoreForm.addEventListener('submit', async (e) => {
+      e.preventDefault()
+      try {
+        const formElem = document.getElementById("createStoreForm")
+
+        const formData = new FormData(formElem)
+
+        const obj = {}
+        formData.forEach((value, key) => obj[key] = value)
+        const data = JSON.stringify({store: obj})
+        debugger
+        let configObj = {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: data 
+        };
+
+        const response = await fetch(createStoreUrl, configObj)
+        const payload = await response.json()
+        console.log(payload)
+      } catch (err) {
+        console.log(err)
+        alert("Error. Set up Alert Messages")
+      }
     })
   }
 }
